f59f83a3138da6cb579fac3efe2d96005a7d3914
Dav Glass <davglass@gmail.com>
2010-04-02 17:35:21 -0700

    Cleaned up source, fixed issue with single tag elements and now almost passes jslint

--- node-htmlparser.js
+++ node-htmlparser.js
@@ -1,54 +1,57 @@
-var sys = require("sys");
+/*global exports, require */
 
-var ElementType = {
-	  Text: "text"
-	, Tag: "tag"
-	, Directive: "directive"
-	, Comment: "comment"
-	, Script: "script"
-	, Style: "style"
-}
+var sys = require('sys'),
+    ElementType = {
+        Text: 'text',
+        Tag: 'tag',
+        Directive: 'directive',
+        Comment: 'comment',
+        Script: 'script', 
+        Style: 'style'
+    },
+    emptyTags = {
+        area: 1,
+        base: 1,
+        basefont: 1,
+        br: 1,
+        col: 1,
+        frame: 1,
+        hr: 1,
+        img: 1,
+        input: 1,
+        isindex: 1,
+        link: 1,
+        meta: 1,
+        param: 1,
+        embed: 1
+    },
+    reTrim = /(^\s+|\s+$)/gm,
+    reTrimTag = /\s*\/\s*$/gm,
+    reTrimEndTag = /^\s*\/\s*/gm,
+    reTrimComment = /(^\!--|--$)/g;
 
-var emptyTags = {
-	  area: 1
-	, base: 1
-	, basefont: 1
-	, br: 1
-	, col: 1
-	, frame: 1
-	, hr: 1
-	, img: 1
-	, input: 1
-	, isindex: 1
-	, link: 1
-	, meta: 1
-	, param: 1
-	, embed: 1
-}
-
-var reTrim = /(^\s+|\s+$)/gm;
-var reTrimTag = /\s*\/\s*$/gm;
-var reTrimEndTag = /^\s*\/\s*/gm;
-var reTrimComment = /(^\!--|--$)/g;
 
 function StringTagFind (data, offset) {
-	var start = data.indexOf("<", offset);
-	var end = data.indexOf(">", offset);
+	var start, end;
+    start = data.indexOf("<", offset);
+	end = data.indexOf(">", offset);
 	return((start < end) ? start : end);
 }
 
 function NestTags (elements) {
-	var nested = [];
+	var nested = [],
+	    idxEnd = elements.length,
+	    idx = 0,
+        element, baseName, pos,
+	    tagStack = [];
 
-	var idxEnd = elements.length;
-	var idx = 0;
-	var tagStack = [];
 	tagStack.last = function last () {
 		return(this.length ? this[this.length - 1] : null);
-	}
+	};
 
 	while (idx < idxEnd) {
-		var element = elements[idx++];
+		element = elements[idx++];
+
 
 		if (!tagStack.last()) {
 			if (element.type != ElementType.Text && element.type != ElementType.Comment && element.type != ElementType.Directive) {
@@ -58,34 +61,39 @@ function NestTags (elements) {
 						tagStack.push(element);
 					}
 				}
-			}
-			else
+			} else {
 				nested.push(element);
-		}
-		else {
+            }
+		} else {
+            //sys.puts('nest: ' + element.data);
 			if (element.type != ElementType.Text && element.type != ElementType.Comment && element.type != ElementType.Directive) {
+                //sys.puts('nest1: ' + element.data);
 				if (element.name[0] == "/") {
-					var baseName = element.name.substring(1);
+					baseName = element.name.substring(1);
 					if (!emptyTags[baseName]) {
-						var pos = tagStack.length - 1;
+						pos = tagStack.length - 1;
 						while (pos > -1 && tagStack[pos--].name != baseName) { }
-						if (pos > -1 || tagStack[0].name == baseName)
-							while (pos < tagStack.length - 1)
+						if (pos > -1 || tagStack[0].name == baseName) {
+							while (pos < tagStack.length - 1) {
 								tagStack.pop();
+                            }
+                        }
 					}
-				}
-				else {
-					if (!emptyTags[element.name]) {
-						if (!tagStack.last().children)
+				} else {
+                    //sys.puts('nest2: ' + element.data);
+					//if (!emptyTags[element.name]) {
+						if (!tagStack.last().children) {
 							tagStack.last().children = [];
+                        }
 						tagStack.last().children.push(element);
 						tagStack.push(element);
-					}
+					//}
 				}
-			}
-			else {
-				if (!tagStack.last().children)
+			} else {
+                //sys.puts('nest3: ' + element.data);
+				if (!tagStack.last().children) {
 					tagStack.last().children = [];
+                }
 				tagStack.last().children.push(element);
 			}
 		}
@@ -95,38 +103,42 @@ function NestTags (elements) {
 }
 
 function ParseAttribs (element) {
-	var tagName = element.data.split(/\s/, 1)[0];
-	var attribRaw = element.data.substring(tagName.length);
-	if (attribRaw.length < 1)
+	var tagName = element.data.split(/\s/, 1)[0],
+        reAttrib, match,
+	    attribRaw = element.data.substring(tagName.length);
+	
+    if (attribRaw.length < 1) {
 		return;
+    }
 
-	var reAttrib =
-		/([^=<>\"\'\s]+)\s*=\s*"([^"]*)"|([^=<>\"\'\s]+)\s*=\s*'([^']*)'|([^=<>\"\'\s]+)\s*=\s*([^'"\s]+)|([^=<>\"\'\s\/]+)/g;
+	reAttrib = /([^=<>\"\'\s]+)\s*=\s*"([^"]*)"|([^=<>\"\'\s]+)\s*=\s*'([^']*)'|([^=<>\"\'\s]+)\s*=\s*([^'"\s]+)|([^=<>\"\'\s\/]+)/g;
 
-	var match;
 	while (match = reAttrib.exec(attribRaw)) {
-		if (!element.attribs)
+		if (!element.attribs) {
 			element.attribs = {};
+        }
 
-		if (typeof match[1] == "string")
+		if (typeof match[1] == "string") {
 			element.attribs[match[1]] = match[2];
-		else if (typeof match[3] == "string")
+		} else if (typeof match[3] == "string") {
 			element.attribs[match[3]] = match[4];
-		else if (typeof match[5] == "string")
+		} else if (typeof match[5] == "string") {
 			element.attribs[match[5]] = match[6];
-		else if (typeof match[7] == "string")
+		} else if (typeof match[7] == "string") {
 			element.attribs[match[7]] = match[7];
+        }
 	}
 }
 
 function ParseTagAttribs (elements) {
-	var idxEnd = elements.length;
-	var idx = 0;
+	var idxEnd = elements.length,
+	    idx = 0, element;
 
 	while (idx < idxEnd) {
-		var element = elements[idx++];
-		if (element.type != ElementType.Tag && element.type != ElementType.Script && element.type != ElementType.style)
+		element = elements[idx++];
+		if (element.type != ElementType.Tag && element.type != ElementType.Script && element.type != ElementType.style) {
 			continue;
+        }
 		ParseAttribs(element);
 	}
 
@@ -138,93 +150,90 @@ function ParseTagName (data) {
 }
 
 function ParseTags (data) {
-	var elements = [];
-
-	var reTags = /[\<\>]/g;
-
-	var current = 0;
-	var next = 0;
-	var end = data.length - 1;
-	var state = ElementType.Text;
-	var tagStack = [];
+	var elements = [],
+	    reTags = /[\<\>]/g,
+        current = 0,
+        next = 0,
+        end = data.length - 1,
+        state = ElementType.Text,
+        tagSep, rawData, element, elementName, prevElement, rawLen,
+        tagStack = [];
 
 	while (reTags.test(data)) {
 		next = reTags.lastIndex - 1;
-		var tagSep = data[next];
-		var rawData = data.substring(current, next);
+		tagSep = data[next];
+		rawData = data.substring(current, next);
 
-		var element = {
-			  raw: rawData
-			, data: (state == ElementType.Text) ? rawData : rawData.replace(reTrim, "")
-			, type: state
+		element = {
+		    raw: rawData,
+			data: (state == ElementType.Text) ? rawData : rawData.replace(reTrim, ""),
+			type: state
 		};
 
-		var elementName = ParseTagName(element.data);
+		elementName = ParseTagName(element.data);
 		if (tagStack.length) {
 			if (tagStack[tagStack.length - 1] == ElementType.Script) {
-				if (elementName == "/script")
+				if (elementName == "/script") {
 					tagStack.pop();
-				else {
-					if (element.raw.indexOf("!--") != 0) {
+				} else {
+					if (element.raw.indexOf("!--") !== 0) {
 						element.type = ElementType.Text;
 						if (elements.length && elements[elements.length - 1].type == ElementType.Text) {
-							if (element.raw != "") {
-								var prevElement = elements[elements.length - 1];
+							if (element.raw !== "") {
+								prevElement = elements[elements.length - 1];
 								prevElement.raw = prevElement.data = prevElement.raw + element.raw + tagSep;
 								element.raw = element.data = "";
 							}
-						}
-						else
-							if (element.raw != "")
+						} else {
+							if (element.raw !== "") {
 								element.raw = element.data = element.raw + tagSep;
+                            }
+                        }
 					}
 				}
-			}
-			else if (tagStack[tagStack.length - 1] == ElementType.Style) {
-				if (elementName == "/style")
+			} else if (tagStack[tagStack.length - 1] == ElementType.Style) {
+				if (elementName == "/style") {
 					tagStack.pop();
-				else {
-					if (element.raw.indexOf("!--") != 0) {
+				} else {
+					if (element.raw.indexOf("!--") !== 0) {
 						element.type = ElementType.Text;
 						if (elements.length && elements[elements.length - 1].type == ElementType.Text) {
-							if (element.raw != "") {
-								var prevElement = elements[elements.length - 1];
+							if (element.raw !== "") {
+								prevElement = elements[elements.length - 1];
 								prevElement.raw = prevElement.data = prevElement.raw + element.raw + tagSep;
 								element.raw = element.data = "";
 							}
-						}
-						else
-							if (element.raw != "")
+						} else {
+							if (element.raw !== "") {
 								element.raw = element.data = element.raw + tagSep;
+                            }
+                        }
 					}
 				}
-			}
-			else if (tagStack[tagStack.length - 1] == ElementType.Comment) {
-				var rawLen = element.raw.length;
+			} else if (tagStack[tagStack.length - 1] == ElementType.Comment) {
+				rawLen = element.raw.length;
 				if (element.raw[rawLen - 1] == "-" && element.raw[rawLen - 1] == "-" && tagSep == ">") {
 					tagStack.pop();
 					if (elements.length && elements[elements.length - 1].type == ElementType.Comment) {
-						var prevElement = elements[elements.length - 1];
+						prevElement = elements[elements.length - 1];
 						prevElement.raw = prevElement.data = (prevElement.raw + element.raw).replace(reTrimComment, "");
 						element.raw = element.data = "";
 						element.type = ElementType.Text;
-					}
-					else
+					} else {
 						element.type = ElementType.Comment;
-				}
-				else {
+                    }
+				} else {
 					element.type = ElementType.Comment;
 					if (elements.length && elements[elements.length - 1].type == ElementType.Comment) {
-						var prevElement = elements[elements.length - 1];
+						prevElement = elements[elements.length - 1];
 						prevElement.raw = prevElement.data = prevElement.raw + element.raw + tagSep;
 						element.raw = element.data = "";
 						element.type = ElementType.Text;
-					}
-					else
+					} else {
 						element.raw = element.data = element.raw + tagSep;
+                    }
 				}
-			}
-			else {
+			} else {
 				//TODO: ???
 			}
 		}
@@ -232,69 +241,67 @@ function ParseTags (data) {
 		if (element.type == ElementType.Tag) {
 			element.name = elementName;
 			
-			if (element.raw.indexOf("!--") == 0) {
+			if (element.raw.indexOf("!--") === 0) {
 				element.type = ElementType.Comment;
-				delete element["name"];
-				var rawLen = element.raw.length;
-				if (element.raw[rawLen - 1] == "-" && element.raw[rawLen - 2] == "-" && tagSep == ">")
+				delete element.name;
+				rawLen = element.raw.length;
+				if (element.raw[rawLen - 1] == "-" && element.raw[rawLen - 2] == "-" && tagSep == ">") {
 					element.raw = element.data = element.raw.replace(reTrimComment, "");
-				else {
+				} else {
 					element.raw += tagSep;
 					tagStack.push(ElementType.Comment);
 				}
-			}
-			else if (element.raw.indexOf("!") == 0) {
+			} else if (element.raw.indexOf("!") === 0) {
 				element.type = ElementType.Directive;
 				//TODO: what about CDATA?
-			}
-			else if (element.name == "script") {
+			} else if (element.name == "script") {
 				element.type = ElementType.Script;
-				if (element.data[element.data.length - 1] != "/")
+				if (element.data[element.data.length - 1] != "/") {
 					tagStack.push(ElementType.Script);
-			}
-			else if (element.name == "/script")
+                }
+			} else if (element.name == "/script") {
 				element.type = ElementType.Script;
-			else if (element.name == "style") {
+			} else if (element.name == "style") {
 				element.type = ElementType.Style;
-				if (element.data[element.data.length - 1] != "/")
+				if (element.data[element.data.length - 1] != "/") {
 					tagStack.push(ElementType.Style);
-			}
-			else if (element.name == "/style")
+                }
+			} else if (element.name == "/style") {
 				element.type = ElementType.Style;
-			if (element.name && element.name[0] == "/")
+            }
+			if (element.name && element.name[0] == "/") {
 				element.data = element.name;
+            }
 		}
 
-		if (element.raw != "" || element.type != ElementType.Text) {
+		if (element.raw !== "" || element.type != ElementType.Text) {
 			elements.push(element);
-			if (
-				element.type != ElementType.Text
-				&&
-				element.type != ElementType.Comment
-				&&
-				element.type != ElementType.Directive
-				&&
-				element.data[element.data.length - 1] == "/"
-				)
+			if (element.type != ElementType.Text &&
+				element.type != ElementType.Comment &&
+				element.type != ElementType.Directive &&
+				element.data[element.data.length - 1] == "/" ) {
 				elements.push({
-					  raw: "/" + element.name
-					, data: "/" + element.name
-					, name: "/" + element.name
-					, type: element.type
+				    raw: "/" + element.name,
+					data: "/" + element.name,
+					name: "/" + element.name,
+				    type: element.type
 				});
+            }
 		}
 		state = (tagSep == "<") ? ElementType.Tag : ElementType.Text;
 		current = next + 1;
 	}
 	if (current < end) {
-		var rawData = data.substring(current);
-		var element = {
-			  raw: rawData
-			, data: (state == ElementType.Text) ? rawData : rawData.replace(reTrim, "")
-			, type: state
-			};
-		if (state == ElementType.Tag || state == ElementType.Script || state == ElementType.Style)
+		rawData = data.substring(current);
+		element = {
+			raw: rawData,
+			data: (state == ElementType.Text) ? rawData : rawData.replace(reTrim, ""),
+		    type: state
+		};
+
+		if (state == ElementType.Tag || state == ElementType.Script || state == ElementType.Style) {
 			element.name = ParseTagName(element.data);
+        }
 		elements.push(element);
 	}
 
@@ -303,6 +310,6 @@ function ParseTags (data) {
 
 exports.ParseHtml = function ParseHtml (data) {
 	return(NestTags(ParseTagAttribs(ParseTags(data))));
-}
+};
 
 exports.ElementType = ElementType;
